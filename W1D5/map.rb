df154244d05a0423e5265a27ee67f20f8b71bcb6
Niyati Desai <niyatihd@gmail.com>
2019-01-10 23:11:18 -0800

    Add #initialize for Mymap class in map.rb

---
+++ W1D5/map.rb
@@ -0,0 +1,24 @@
+class Mymap
+  def self.generate
+    Hash.new { |h, k|  }
+  end
+
+  def initialize()
+  end
+
+  def set(key, val)
+
+  end
+
+  def get(key)
+
+  end
+
+  def delete(key)
+
+  end
+
+  def show
+
+  end
+end
