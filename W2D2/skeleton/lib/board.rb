67001908aa1aee3676dfd20b1b0b65c5cf09460e
Niyati Desai <niyatihd@gmail.com>
2019-01-15 07:54:21 -0800

    Update #initialize #valid_move #make_move methods of Board class

--- W2D2/skeleton/lib/board.rb
+++ W2D2/skeleton/lib/board.rb
@@ -1,5 +1,5 @@
 class Board
-  attr_accessor :cups
+  attr_accessor :cups, :name1, :name2
 
   def initialize(name1, name2)
     @name1 = name1
@@ -27,10 +27,33 @@ class Board
   def make_move(start_pos, current_player_name)
     stones = @cups[start_pos]
     @cups[start_pos] = []
+
+    ending_cup_idx = start_pos + 1
+    until stones.empty?
+      if current_player_name == name1
+        @cups[ending_cup_idx] += [:stone] unless (ending_cup_idx) == 13
+      else
+        @cups[ending_cup_idx] += [:stone] unless (ending_cup_idx) == 6
+      end
+
+      stones.pop
+      ending_cup_idx = (ending_cup_idx + 1) % 13
+    end
+
+    render
+    next_turn(ending_cup_idx)
   end
 
   def next_turn(ending_cup_idx)
-    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    # helper method to determine whether #make_move returns :switch,
+    #  :prompt, or ending_cup_idx
+    if cups[ending_cup_idx].count == 1
+      return :switch
+    elsif ending_cup_idx == 6 || ending_cup_idx == 13
+      return :prompt
+    else
+      return ending_cup_idx
+    end
   end
 
   def render
@@ -42,8 +65,18 @@ class Board
   end
 
   def one_side_empty?
+    cups[0..5].all? { |el| el == [] } || cups[7..12].all? { |el| el == [] }
   end
 
   def winner
+    result = cups[6].length <=> cups[13].length
+    case result
+    when 0
+      return :draw
+    when 1
+      return name1
+    when -1
+      return name2
+    end
   end
 end
