b003f25393e2bc76d3b8624c56907d9b1b46787a
Niyati Desai <niyatihd@gmail.com>
2019-01-17 09:01:49 -0800

    Add #clever_octopus

--- W2D4/bigO_exercise.rb
+++ W2D4/bigO_exercise.rb
@@ -1,7 +1,7 @@
 def sluggish_octopus(arr)
   arr.length.times do |i|
     longest = true
-    
+
     arr.length.times do |j|
       next if i == j
       if arr[i].length < arr[j].length
@@ -14,3 +14,17 @@ def sluggish_octopus(arr)
 end
 
 p sluggish_octopus(['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh'])
+
+def clever_octopus(arr)
+  longest = ''
+  
+  arr.length.times do |i|
+    if arr[i].length > longest.length
+      longest = arr[i]
+    end
+  end
+  
+  return longest
+end
+
+p clever_octopus(['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh'])
